c45bff4d58a6c517bac03482d139171849855cd7
NTPT-DEV <Nt.nuttaphong@gmail.com>
2025-06-11 01:37:05 +0700

    Fixed error midleware -3

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,24 @@
 import authConfig from "@/auth.config";
 import NextAuth from "next-auth";
 import { privateRoutes } from "./utils/routesUrl";
-// import { getToken } from "next-auth/jwt";
+import { getToken } from "next-auth/jwt";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth(async function middleware(req) {
-  // GET all data from token 
-  // const token = await getToken({ req , secret : process.env.AUTH_SECRET });
-  const roleUser = req.auth?.user.role
+  // GET all data from token
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const roleUser = token?.role;
+
+  const testAuthSecretCheck  = process.env.AUTH_SECRET
+  const testAuthSecretCheckv2  = process.env.AUTH_SECRET!
+
+  console.log(testAuthSecretCheck, "This is testAuthSecretCheck") 
+  console.log(typeof testAuthSecretCheck, "This is testAuthSecretCheck Type") 
+  console.log(typeof testAuthSecretCheckv2, "This is testAuthSecretCheck V2 Type") 
+
+  
+
   const isLoggedIn = !!req.auth;
 
   const { nextUrl } = req;
@@ -19,12 +29,13 @@ export default auth(async function middleware(req) {
   const isApiRoute = nextUrl.pathname.includes("/api");
   // const idDashBoard = nextUrl.pathname.includes("/dashboard");
 
-  if (isApiRoute && roleUser !== 'admin') {
+  if (isApiRoute && roleUser !== "admin") {
     return Response.redirect(`${url}`);
   }
-  console.log(url , 'This is Url');
-  console.log(roleUser , 'This is roleUser');
-  console.log(isLoggedIn , 'This is isLoggedIn');
+  console.log(url, "This is Url");
+  console.log(roleUser, "This is roleUser");
+  console.log(token, "This is token");
+  console.log(isLoggedIn, "This is isLoggedIn");
 
   if (isLoggedIn && isAuthRoute) {
     return Response.redirect(`${url}`);
@@ -37,10 +48,7 @@ export default auth(async function middleware(req) {
   // if(isLoggedIn && idDashBoard && roleUser !== 'admin') {
   //   return Response.redirect(`${url}`);
   // }
-
 });
-
-
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
