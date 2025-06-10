588ab443ee462ac83b508d170acb39dea4a8c1fb
NTPT-DEV <Nt.nuttaphong@gmail.com>
2025-06-10 22:37:51 +0700

    Fixed Deploy -12

--- src/app/(main)/checkout/page.tsx
+++ src/app/(main)/checkout/page.tsx
@@ -1,31 +1,9 @@
-"use client"
-import {
-  EmbeddedCheckout,
-  EmbeddedCheckoutProvider,
-} from "@stripe/react-stripe-js";
-
-import { loadStripe } from "@stripe/stripe-js";
-import { useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-);
-
 const CheckoutPage = () => {
-  const searchParams = useSearchParams();
-  const clientSecretData = searchParams.get("clientSecret");
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <EmbeddedCheckoutProvider
-          stripe={stripePromise}
-          options={{ clientSecret: clientSecretData }}
-        >
-          <EmbeddedCheckout />
-        </EmbeddedCheckoutProvider>
-      </div>
+      <CheckoutPage />
     </Suspense>
   );
 };
